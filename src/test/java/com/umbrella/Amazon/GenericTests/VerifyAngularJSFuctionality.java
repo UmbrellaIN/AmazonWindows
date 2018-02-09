56c3585ff9f31067c04f0aba0f12e91945594ede
ankitworld08 <ankitworld08@gmail.com>
2018-02-09 16:48:59 +0530

    commit0209

---
+++ src/test/java/com/umbrella/Amazon/GenericTests/VerifyAngularJSFuctionality.java
@@ -0,0 +1,23 @@
+package com.umbrella.Amazon.GenericTests;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import com.umbrella.Amazon.generics.LoggerHelper;
+import com.umbrella.Amazon.testCore.Config;
+import com.umbrella.Amazon.testCore.TestBase;
+
+public class VerifyAngularJSFuctionality extends TestBase {
+	
+	Config config;
+	private static final Logger log = LoggerHelper.getLogger(VerifyAngularJSFuctionality.class);
+	
+	@Test
+	public void test1()
+	{
+		log.info(VerifyAngularJSFuctionality.class.getName() + " Method test1 get Started");
+		config = new Config(OR);
+		driver.get(config.getWebsite());
+	}
+
+}
